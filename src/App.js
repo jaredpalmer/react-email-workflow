3c31810ba2064ba471f453970db286861b872008
Jared Palmer <palmdeezy>
2016-01-26 14:42:19 -0500

    jsxstyle now works with react transform hmr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import {Block} from 'jsxstyle';
+import L from './LayoutConstants';
 
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hovered: false
+    };
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+  handleMouseEnter() {
+     this.setState({hovered: true});
+  }
+
+  handleMouseLeave() {
+     this.setState({hovered: false});
+  }
+
   render() {
     return (
-      <div>
-        'Hello World'
+       <div role="button" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+          <Block
+            marginLeft={L.gridUnit}
+            color={this.state.hovered ? 'white' : 'black'}>
+            Hello Worldssss
+          </Block>
       </div>
     );
   }
---
+++ src/LayoutConstants.js
@@ -0,0 +1,13 @@
+
+var Color = require('jsxstyle/lib/Color');
+
+var primaryColor = Color.rgb(10, 0, 0);
+var secondaryColor = Color.alpha(primaryColor, .8);
+
+var LayoutConstants = {
+  primaryColor: primaryColor,
+  secondaryColor: secondaryColor,
+  gridUnit: 8,
+}
+
+module.exports = LayoutConstants;
