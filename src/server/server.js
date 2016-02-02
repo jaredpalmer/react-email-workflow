b3af8fdb483786f2756bd37366fe7c2597679c72
Jared Palmer <palmdeezy>
2016-02-01 19:45:13 -0500

    changed port binding syntax

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,6 @@ const compression = require('compression');
 const helmet = require('helmet');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
 const app = express();
 
 app.disable('x-powered-by');
@@ -19,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v0/premail', require('./api/premail'));
 app.use('/api/v0/extract', require('./api/extract'));
 app.get('/check', (req, res)=> res.send('Everything is awesome!'));
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000), () => {
 	// console.log(process.env);
   console.log('Listening on ' + port + '.');
   console.log('Go to <http://localhost:' + port + '> in your browser.');
