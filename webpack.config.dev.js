ad6292135c42265814200fabaae4d6e7bb6314af
Jared Palmer <palmdeezy>
2016-02-08 13:21:52 -0500

    removing nodemon allows for HMR

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'eval',
   entry: [
     // 'webpack-dev-server/client?http://localhost:3000',
-    // 'webpack/hot/only-dev-server',
+  'webpack/hot/only-dev-server',
     'webpack-hot-middleware/client?reload=true',
     './src/index'
   ],
