ee8ca4dac8a98b0373081fde452a8c4f58db4b08
Jared Palmer <palmdeezy>
2016-02-14 14:10:02 -0500

    Create reusable type styles

--- src/components/PrimaryText.js
+++
@@ -1,6 +0,0 @@
-import { curry, Inline} from 'jsxstyle';
-import L from '../LayoutConstants';
-
-const PrimaryText = curry(Inline, { fontSize: '2rem', fontWeight: 'bold', color: L.white });
-
-export default PrimaryText;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,20 +4,22 @@ import Preview from './Preview';
 import Meta from './Meta';
 import ElementList from './ElementList';
 import L from '../LayoutConstants';
-import PrimaryText from '../components/PrimaryText';
+import { H3 } from '../components/Type';
 
 export default class App extends Component {
   render() {
     return (
       <Col>
-        <Block height={L.gridUnit * 6.25}
-               position="fixed"
-               top="0"
-               left="0"
-               width="100%"
-               background={L.black}>
-            <PrimaryText>React Email Workflow</PrimaryText>
-        </Block>
+        <Row height={L.gridUnit * 6.25}
+             alignItems="center"
+             position="fixed"
+             top="0"
+             left="0"
+             width="100%"
+             padding="0 1rem"
+             background={L.black}>
+            <H3 color="#ffffff">React Email Workflow</H3>
+        </Row>
         <Block maxWidth="616px"
                background="#f2f2f2"
                borderRight="1px solid #c4c4c4"
