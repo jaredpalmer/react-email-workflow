b8c77cbd24d672f2ec9d00ff5762a60b58b5457c
Jared Palmer <palmdeezy>
2016-02-15 16:15:33 -0500

    try to fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,10 @@
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
-var PROD = process.env.NODE_ENV == "production";
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function(x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function(mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
-
-module.exports = [{
+module.exports = {
   name: 'client',
+  devtool: false,
   entry: [
     './src/index'
   ],
@@ -23,77 +14,30 @@ module.exports = [{
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel!' + path.join(__dirname, 'node_modules/jsxstyle/lib/webpackLoader.js') + '?LayoutConstants=' + path.join(__dirname, 'src', 'LayoutConstants.js'),
+      include: path.join(__dirname, 'src')
+    }]
+  },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       },
       __DEV__: false
     }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      compressor: {
+      compress: {
+        screw_ie8: true,
         warnings: false
       }
-    })
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
   ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel?presets[]=es2015&presets[]=react&presets[]=stage-0!' + path.join(__dirname, 'node_modules/jsxstyle/lib/webpackLoader.js') + '?LayoutConstants=' + path.join(__dirname, 'src', 'LayoutConstants.js'),
-      include: path.join(__dirname, 'src')
-    }]
-  }
-}, {
-    name: 'server',
-    cache: false,
-    debug: false,
-    profile: false,
-    bail: true,
-    target: 'node',
-    entry: './server.js',
-    // devtool: 'sourcemap',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'server.bundle.js',
-        libraryTarget: 'commonjs2',
-    },
-    externals: nodeModules,
-    node: {
-      console: false,
-      global: false,
-      process: false,
-      Buffer: false,
-      __filename: false,
-      __dirname: false,
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react', 'stage-0']
-                }
-            }
-        ]
-    },
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js', '.jsx', '.json']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-              'NODE_ENV': JSON.stringify('production')
-            },
-            __CLIENT__: false,
-            __SERVER__: true,
-            __DEV__: false
-        }),
-        new webpack.BannerPlugin('require("source-map-support").install();',
-            { raw: true, entryOnly: false })
-    ]
-}];
+
+};
