1bd3ca7edc7ec5c425a185ebae9cda45c1e9001c
Jared Palmer <palmdeezy>
2016-02-15 19:02:02 -0500

    tighten up margins on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -73,7 +73,7 @@ class Card extends Component {
           <Block
             background="#fff"
             width="100%"
-            margin={'1rem 0'}
+            margin={'.5rem 0'}
             boxShadow={this.state.hovered ? '0 4px 4px 0 rgba(0,0,0,.12)' : '0 2px 2px 0 rgba(0,0,0,.08)' }
             transition=".2s box-shadow cubic-bezier(0.4, 0, 0.2, 1)"
             padding="1rem"
