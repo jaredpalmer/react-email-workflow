42a80de836afd93654e4085e1ed179b26fba7ebe
Jared Palmer <jaredpalmer>
2016-06-02 17:25:14 -0400

    upgrade to react-hot-loader v3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './containers/App';
+import { AppContainer } from 'react-hot-loader';
+import Root from './containers/Root';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+const rootEl = document.getElementById('root')
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root')
+  <AppContainer>
+    <Root store={store} />
+  </AppContainer>,
+  rootEl
 );
+
+
+if (module.hot) {
+   module.hot.accept('./containers/Root', () => {
+     // If you use Webpack 2 in ES modules mode, you can
+     // use <Assignments /> here rather than require() a <NextAssignments />.
+     const NextRoot = require('./containers/Root').default;
+     ReactDOM.render(
+       <AppContainer>
+         <Root store={store}/>
+       </AppContainer>,
+       creationComponentContainer
+     );
+   });
+}
