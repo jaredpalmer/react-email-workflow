b884f31bee9554b70396870b78a012e8945b769a
Jared Palmer <jaredpalmer>
2016-06-02 19:19:47 -0400

    remove warning

--- src/index.js
+++ src/index.js
@@ -11,16 +11,3 @@ ReactDOM.render(
     <App />
   </Provider>, document.getElementById('root')
 );
-
-window.onbeforeunload = function (event) {
-  var message = 'Sure you want to leave?';
-  if (typeof event === 'undefined') {
-    event = window.event;
-  }
-
-  if (event) {
-    event.returnValue = message;
-  }
-
-  return message;
-};
