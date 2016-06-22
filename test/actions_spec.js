148a48f9ed9758c0759dd9f2a774ec6f685b7f91
Jared Palmer <jaredpalmer>
2016-06-22 15:33:12 -0400

    refactor tests to standardjs

--- test/actions_spec.js
+++ test/actions_spec.js
@@ -1,7 +1,8 @@
-import expect from 'expect';
-import * as actions from '../src/actions/ElementActions';
-import * as types from '../src/constants/ActionTypes';
-import * as ElementSchema from '../src/constants/ElementSchema';
+/* eslint-env mocha */
+import expect from 'expect'
+import * as actions from '../src/actions/ElementActions'
+import * as types from '../src/constants/ActionTypes'
+import * as ElementSchema from '../src/constants/ElementSchema'
 import { v4 } from 'node-uuid'
 
 describe('actions', () => {
@@ -16,36 +17,36 @@ describe('actions', () => {
         content: '',
         author: ''
       }
-    };
+    }
     const createdAction = actions.add(ElementSchema.url)
-    expect(createdAction.element.id).toBeA('string');
+    expect(createdAction.element.id).toBeA('string')
     createdAction.element.id = 1
-    expect(createdAction).toEqual(expectedAction);
-  });
+    expect(createdAction).toEqual(expectedAction)
+  })
 
   it('should create an action to edit an element', () => {
     const id = v4()
     const updates = {
       url: 'http://google.com'
-    };
+    }
     const expectedAction = {
       type: types.EDIT_ELEMENT,
       id,
       updates: {
         url: 'http://google.com'
       }
-    };
-    expect(actions.edit(id, updates)).toEqual(expectedAction);
-  });
+    }
+    expect(actions.edit(id, updates)).toEqual(expectedAction)
+  })
 
   it('should create an action to destroy an element', () => {
     const id = v4()
     const expectedAction = {
       type: types.DESTROY_ELEMENT,
-      id,
-    };
-    expect(actions.destroy(id)).toEqual(expectedAction);
-  });
+      id
+    }
+    expect(actions.destroy(id)).toEqual(expectedAction)
+  })
 
   it('should create an action to move an element', () => {
     const id = v4()
@@ -53,15 +54,15 @@ describe('actions', () => {
       type: types.MOVE_ELEMENT,
       id,
       atIndex: 1
-    };
-    expect(actions.move(id,1)).toEqual(expectedAction);
-  });
+    }
+    expect(actions.move(id, 1)).toEqual(expectedAction)
+  })
 
   it('should create an action to edit meta data', () => {
     const expectedAction = {
       type: types.EDIT_META,
       meta: {subject: 'Apple goes Bankrupt'}
-    };
-    expect(actions.meta({subject: 'Apple goes Bankrupt'})).toEqual(expectedAction);
-  });
-});
+    }
+    expect(actions.meta({subject: 'Apple goes Bankrupt'})).toEqual(expectedAction)
+  })
+})
