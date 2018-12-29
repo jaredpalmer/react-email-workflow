7bfe7d1c2d7a146c0b15d5e7781cb6ccfa5e6ea7
Jared Palmer <jared@palmer.net>
2018-12-29 09:43:50 -0500

    Add back port binding

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const web = require('./web');
 
 const RABBIT_URL =
   process.env.CLOUDAMQP_URL || 'amqp://guest:guest@localhost:5672';
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const __DEV__ = process.env.NODE_ENV === 'development';
 
