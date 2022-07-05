3d7f6b5fbb147d6e6e967a2145694fe7217cda59
Jared Palmer <jared@jaredpalmer.com>
2022-07-05 13:15:19 -0400

    Add preset for metacademy (#238)

--- client/reducers/meta.js
+++ client/reducers/meta.js
@@ -1,15 +1,19 @@
-import * as types from '../constants/ActionTypes'
-import update from 'react/lib/update'
+import * as types from '../constants/ActionTypes';
+import update from 'react/lib/update';
 
-export default function meta (state = {
-  subject: '',
-  preheader: '',
-  date: new Date()
-}, action) {
+export default function meta(
+  state = {
+    preset: 'shelly',
+    subject: '',
+    preheader: '',
+    date: new Date(),
+  },
+  action
+) {
   switch (action.type) {
     case types.EDIT_META:
-      return update(state, { $merge: action.meta })
+      return update(state, { $merge: action.meta });
     default:
-      return state
+      return state;
   }
 }
